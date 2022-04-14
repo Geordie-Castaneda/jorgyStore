4a65a8ca58fb08e4f887e57920f80b4034362f36
jorgy <jordyorantes9@gmail.com>
2022-04-14 16:23:44 -0600

    proudcto agregada

---
+++ migrations/Version20220414222217.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220414222217 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function preUp(Schema $schema): void
+    {
+        parent::preUp($schema);
+
+        $this->addSql("ALTER session SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS' NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS' NLS_TIMESTAMP_TZ_FORMAT = 'YYYY-MM-DD HH24:MI:SS TZH:TZM'");
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE product_id_seq START WITH 1 MINVALUE 1 INCREMENT BY 1');
+        $this->addSql('CREATE TABLE product (id NUMBER(10) NOT NULL, name VARCHAR2(255) NOT NULL, description VARCHAR2(255) NOT NULL, price DOUBLE PRECISION NOT NULL, created_by VARCHAR2(255) NOT NULL, updated_by VARCHAR2(255) NOT NULL, created_at TIMESTAMP(0) NOT NULL, updated_at TIMESTAMP(0) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE product_id_seq');
+        $this->addSql('DROP TABLE product');
+    }
+}
